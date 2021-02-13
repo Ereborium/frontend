eeb6d1d7a8233c4dc31ce5edb6ced27f9e207f7f
nezouse <stankiewicz1998@gmail.com>
2021-02-13 11:12:43 +0100

    Fix issues

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from "next/app";
 import { FC } from "react";
 import "../styles/globals.css";
 
-const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
+const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
 
