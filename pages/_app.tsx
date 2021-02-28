826ae19d9ea142767443a4839c74d478213f2620
ShellModule <szymon.sledziewski98@gmail.com>
2021-02-28 12:16:14 +0100

    Setup chakra-ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
 import { FC } from "react";
 import "../styles/globals.css";
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />;
+    </ChakraProvider>
+  );
 };
 
 export default MyApp;
